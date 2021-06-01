994bf51c73be2739f3b68ae8fbc72ab52681fefd
JCapelDev <win200003@gmail.com>
2021-06-01 19:55:10 +0200

    images added

--- app/src/main/java/com/capel/galery/CustomAdapter.java
+++ app/src/main/java/com/capel/galery/CustomAdapter.java
@@ -1,7 +1,9 @@
 package com.capel.galery;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,31 +11,44 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
     private String[] items;
+    private int[] programImages = {
+            R.drawable.h6jJMYjltWk,
+            R.drawable.jadMxMbKbiQ,
+            R.drawable.Jt4ABNbETW4,
+            R.drawable.oGTvZCs3GhI,
+            R.drawable.u1Re1UAer7g,
+            R.drawable.u7XAOL8N_3U,
+            R.drawable.wrr6bqkJiD0,
+            R.drawable.XXt6wwDWNEM
+    };
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final ImageView imageView;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
 
-            textView = (TextView) view.findViewById(R.id.textView);
+            imageView = (ImageView) view.findViewById(R.id.imageView);
         }
 
-        public TextView getTextView() {
-            return textView;
+        public ImageView getImageView() {
+            return imageView;
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.custom_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.getImageView().setImageResource(position);
     }
 
     @Override
